d632a1f638b5048979c471f03372e190f6cebd52
roerjo <roerjo.personal@gmail.com>
2017-02-04 11:08:41 -0500

    sum a range problem

---
+++ ch4/sumRange.js
@@ -0,0 +1,43 @@
+/*
+Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+
+Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the previous program and see whether it does indeed return 55.
+
+As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used to build up the array. If no step is given, the array elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+*/
+
+//My solution:
+
+function range(start, end, step) {
+    var list = [];
+
+    if(step > 0) {
+        for (var i = start; i <= end; i += step) {
+            list.push(i);
+        }
+    }
+    else if (step < 0){
+        step = Math.abs(step);
+        for (var j = start; j >= end; j -= step) {
+            list.push(j);
+        }
+    }
+    else {
+        for (var k = start; k <= end; k++) {
+            list.push(k);
+        }
+    }
+      
+    return list;
+}
+
+function sum(arr) {
+    var result = 0;
+    for (var i = 0; i < arr.length; i++) {
+	result += arr[i];
+    }
+    return result;
+}
+
+console.log(sum(range(1, 10, 2)));
+console.log(range(5, 2, -1));
