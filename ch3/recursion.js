80dae54dc9df1928b84140c05207f522f24aca06
Eric Johnson <roerjo.personal@gmail.com>
2018-04-29 14:17:01 -0400

    update recursion

--- ch3/recursion.js
+++ ch3/recursion.js
@@ -10,16 +10,19 @@ Define a recursive function isEven corresponding to this description. The functi
 */
 
 function isEven(num) {
-  if (num == 0)
-    return true;
-  else if (num == 1)
-    return false;
-  else if (num > 1)
-    return isEven(num - 2);
-  else 
-    return isEven(num + 2);
+  if (num === 0) return true;
+  if (num === 1 || num === -1) return false;
+  if (num < -1) return isEven(num + 2);
+  return isEven(num - 2);
 }
 
-console.log(isEven(50));
-console.log(isEven(75));
-console.log(isEven(-5));
+/*
+    Eloquent Solution:
+
+    function isEven(n) {
+        if (n = 0) return true;
+        else if (n == 1) return false;
+        else if (n < 0) return isEven(-n);
+        else return isEven(n - 2);
+    }
+*/
