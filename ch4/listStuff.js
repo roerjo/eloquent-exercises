091a13d10f5aa63be61aabb25ec17fa37b61063b
Eric <roerjo.personal@gmail.com>
2018-05-06 13:22:10 -0400

    Update listStuff.js

--- ch4/listStuff.js
+++ ch4/listStuff.js
@@ -1,5 +1,10 @@
 /*
-Write a function arrayToList that builds up a data structure like the previous one when given [1, 2, 3] as argument, and write a listToArray function that produces an array from a list. Also write the helper functions prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list, or undefined when there is no such element.
+Write a function arrayToList that builds up a data structure like the previous one when given
+[1, 2, 3] as argument, and write a listToArray function that produces an array from a list.
+Also write the helper functions prepend, which takes an element and a list and creates a new
+list that adds the element to the front of the input list, and nth, which takes a list and a
+number and returns the element at the given position in the list, or undefined when there is
+no such element.
 
 If you haven’t already, also write a recursive version of nth.
 */
@@ -49,11 +54,6 @@ function nth(list, index) {
   }
 }
 
-console.log(arrayToList([10, 20]));
-console.log(listToArray(arrayToList([10, 20, 30])));
-console.log(prepend(10, prepend(20, null)));
-console.log(nthRecursive(arrayToList([10, 20, 30]), 1));
-
 /*
     Eloquent's Solution:
 
@@ -83,3 +83,8 @@ console.log(nthRecursive(arrayToList([10, 20, 30]), 1));
       else return nth(list.rest, n - 1);
     }
 */
+
+console.log(arrayToList([10, 20]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nthRecursive(arrayToList([10, 20, 30]), 1));
