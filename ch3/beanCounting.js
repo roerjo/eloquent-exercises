a167d5d77358b2383f55e521d95935648e45caa8
Eric <roerjo.personal@gmail.com>
2018-05-06 13:18:37 -0400

    Update beanCounting.js

--- ch3/beanCounting.js
+++ ch3/beanCounting.js
@@ -1,7 +1,10 @@
 /*
-Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters are in the string.
+Write a function countBs that takes a string as its only argument and returns a number that indicates how
+many uppercase “B” characters are in the string.
 
-Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that
+indicates the character that is to be counted (rather than counting only uppercase “B” characters).
+Rewrite countBs to make use of this new function.
 */
 
 function countBs(str) {
@@ -32,3 +35,6 @@ function countChar(str, chr) {
         return counted;
     }
 */
+
+console.log(countBs("BBC"));
+console.log(countChar("kakkerlak", "k"));
