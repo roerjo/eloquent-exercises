b787009b69b9d714ab33bbf9008b0b464f40c9c6
Eric Johnson <roerjo.personal@gmail.com>
2018-05-06 12:16:44 -0400

    deep update to es6

--- ch4/deepComp.js
+++ ch4/deepComp.js
@@ -4,36 +4,20 @@ Write a function, deepEqual, that takes two values and returns true only if they
 To find out whether to compare two things by identity (use the === operator for that) or by looking at their properties, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: by a historical accident, typeof null also produces "object".
 */
 
-//My solution...pretty bad:
+//My solution:
 
-function deepEqual(val1, val2) {
+function deepEqual(firstThing, secondThing) {
+  if (firstThing !== null && secondThing !== null &&
+      typeof firstThing === 'object' && typeof secondThing === 'object'
+  ) {
+    for (const key of Object.keys(firstThing)) {
+      return deepEqual(firstThing[key], secondThing[key]);
+    }
+  } else if (firstThing !== secondThing) {
+    return false;
+  }
 
-    if(val1 === val2) {
-        return true;
-    }
-    else if (typeof val1 == "object" && typeof val2 == "object") {
-        var count = 0;
-        for (var prop in val1) {
-            count++;
-        }
-        var count2 = 0;
-        for (var prop in val2) {
-            count2++;
-        }
-        if(count != count2)
-            return false;
-        else {
-            var test = true;
-            for (var prop in val1) {
-                test = deepEqual(val1[prop], val2[prop]);
-                if(!test)
-                  return false;
-            }
-            return true;
-        }
-    }
-    else 
-      return false;
+  return true;
 }
 
 var obj = {here: {is: "an"}, object: 2};
@@ -42,25 +26,21 @@ console.log(deepEqual(obj, {here: 1, object: 2}));
 console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 
 /*
-	Eloquent's solution:
-function deepEqual(a, b) {
-  if (a === b) return true;
-  
-  if (a == null || typeof a != "object" ||
-      b == null || typeof b != "object")
-    return false;
-  
-  var propsInA = 0, propsInB = 0;
+    Eloquent's solution:
+    function deepEqual(a, b) {
+      if (a === b) return true;
 
-  for (var prop in a)
-    propsInA += 1;
+      if (a == null || typeof a != "object" ||
+          b == null || typeof b != "object") return false;
 
-  for (var prop in b) {
-    propsInB += 1;
-    if (!(prop in a) || !deepEqual(a[prop], b[prop]))
-      return false;
-  }
+      let keysA = Object.keys(a), keysB = Object.keys(b);
 
-  return propsInA == propsInB;
-}
+      if (keysA.length != keysB.length) return false;
+
+      for (let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+      }
+
+      return true;
+    }
 */
