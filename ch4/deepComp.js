7d7d6749e54d1fca03e290ce72a8959251007a32
Eric <roerjo.personal@gmail.com>
2018-05-06 13:21:02 -0400

    Update deepComp.js

--- ch4/deepComp.js
+++ ch4/deepComp.js
@@ -1,7 +1,12 @@
 /*
-Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects with the same properties whose values are also equal when compared with a recursive call to deepEqual.
+Write a function, deepEqual, that takes two values and returns true only if they are the same value
+or are objects with the same properties whose values are also equal when compared with a recursive
+call to deepEqual.
 
-To find out whether to compare two things by identity (use the === operator for that) or by looking at their properties, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: by a historical accident, typeof null also produces "object".
+To find out whether to compare two things by identity (use the === operator for that) or by looking
+at their properties, you can use the typeof operator. If it produces "object" for both values, you
+should do a deep comparison. But you have to take one silly exception into account: by a historical
+accident, typeof null also produces "object".
 */
 
 //My solution:
@@ -19,11 +24,6 @@ function deepEqual(firstThing, secondThing) {
 
   return true;
 }
-
-var obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj, obj));
-console.log(deepEqual(obj, {here: 1, object: 2}));
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 
 /*
     Eloquent's solution:
@@ -44,3 +44,8 @@ console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
       return true;
     }
 */
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
